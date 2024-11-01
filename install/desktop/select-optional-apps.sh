385f7a48cb8ba667a05fe0b38234e60f23ab44dc
Zakary <zakariyyaraji@gmail.com>
2024-11-01 13:41:44 +0100

    Update select-optional-apps.sh

--- install/desktop/select-optional-apps.sh
+++ install/desktop/select-optional-apps.sh
@@ -1,9 +1,9 @@
-if [[ -v OMAKUB_FIRST_RUN_OPTIONAL_APPS ]]; then
-	apps=$OMAKUB_FIRST_RUN_OPTIONAL_APPS
+# if [[ -v OMAKUB_FIRST_RUN_OPTIONAL_APPS ]]; then
+# 	apps=$OMAKUB_FIRST_RUN_OPTIONAL_APPS
 
-	if [[ -n "$apps" ]]; then
-		for app in $apps; do
-			source "install/desktop/optional/app-${app,,}.sh"
-		done
-	fi
-fi
+# 	if [[ -n "$apps" ]]; then
+# 		for app in $apps; do
+# 			source "install/desktop/optional/app-${app,,}.sh"
+# 		done
+# 	fi
+# fi
