a27a397e9e3091f7e2829d52136e516c204b739e
Zakary <zakariyyaraji@gmail.com>
2024-11-01 14:08:05 +0100

    Update gnome.sh

--- themes/tokyo-night/gnome.sh
+++ themes/tokyo-night/gnome.sh
@@ -1,3 +1,4 @@
 OMAKUB_THEME_COLOR="purple"
 OMAKUB_THEME_BACKGROUND="tokyo-night/background.jpg"
-source $OMAKUB_PATH/themes/set-gnome-theme.sh
+#source $OMAKUB_PATH/themes/set-gnome-theme.sh
+source /home/overlord/omakub-popos/themes/set-gnome-theme.sh
