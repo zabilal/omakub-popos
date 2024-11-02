bdbcf607c4b5f9b046d48b66e34fbbc2db4b691b
Zakary <zakariyyaraji@gmail.com>
2024-11-02 16:43:49 +0100

    Update app-zellij.sh

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,10 +1,10 @@
-cd /tmp
-wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
-tar -xf zellij.tar.gz zellij
-sudo install zellij /usr/local/bin
-rm zellij.tar.gz zellij
-cd -
+# cd /tmp
+# wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+# tar -xf zellij.tar.gz zellij
+# sudo install zellij /usr/local/bin
+# rm zellij.tar.gz zellij
+# cd -
 
-mkdir -p ~/.config/zellij/themes
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp configs/zellij.kdl ~/.config/zellij/config.kdl
-cp themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
+# mkdir -p ~/.config/zellij/themes
+# [ ! -f "$HOME/.config/zellij/config.kdl" ] && cp configs/zellij.kdl ~/.config/zellij/config.kdl
+# cp themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
