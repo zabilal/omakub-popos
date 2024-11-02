6f84c99d3ded5f0a4814f192745b34b7024df571
Zakary <zakariyyaraji@gmail.com>
2024-11-02 16:33:22 +0100

    Update app-typora.sh

--- install/desktop/app-typora.sh
+++ install/desktop/app-typora.sh
@@ -1,9 +1,9 @@
-wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
-sudo add-apt-repository -y 'deb https://typora.io/linux ./'
-sudo apt update
-sudo apt install -y typora
+# wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
+# sudo add-apt-repository -y 'deb https://typora.io/linux ./'
+# sudo apt update
+# sudo apt install -y typora
 
-# Add iA Typora theme
-mkdir -p ~/.config/Typora/themes
-cp configs/typora/ia_typora.css ~/.config/Typora/themes/
-cp configs/typora/ia_typora_night.css ~/.config/Typora/themes/
+# # Add iA Typora theme
+# mkdir -p ~/.config/Typora/themes
+# cp configs/typora/ia_typora.css ~/.config/Typora/themes/
+# cp configs/typora/ia_typora_night.css ~/.config/Typora/themes/
